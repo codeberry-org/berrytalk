161d32b2fcde3e055f614215cc121c4413d38e24
sungtaek <leesungtaek@gmail.com>
2025-04-22 23:08:31 +0900

    add session manager

---
+++ src/main/java/org/codeberry/berrytalk/chat/domain/Session.java
@@ -0,0 +1,14 @@
+package org.codeberry.berrytalk.chat.domain;
+
+import java.util.function.Consumer;
+
+import org.codeberry.berrytalk.chat.common.model.DeviceType;
+
+public interface Session {
+  String getUserId();
+  String getDeviceId();
+  DeviceType getDeviceType();
+  void sendMessage(Message message);
+  void onClose(Consumer<Session> doOnClose);
+  void close();
+}
